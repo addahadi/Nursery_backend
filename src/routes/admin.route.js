a79c515af2beda872017fde13e73577526ad2b02
addahadi <145640088+addahadi@users.noreply.github.com>
2026-01-09 15:42:59 +0100

    Update admin.route.js

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -24,6 +24,7 @@ import {
   CreateTeacherSchema,
   EditClassRoomSchema,
   EditTeacherSchema,
+  EditAdminSchema,
   FilteredParentListSchema,
   FilteredTeacherListSchema,
   getPaymentsListSchema,
