7be18998d714b10052b93ff6d0660c8b1c09df25
addahadi <145640088+addahadi@users.noreply.github.com>
2026-01-10 23:25:57 +0100

    Verify SMTP connection when server starts
    Add email transporter verification on server start

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import dotenv from 'dotenv/config.js';
-
+import transporter from './utils/email.js'
 import app from './app.js';
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+
+  transporter.verify((err, success) => {
+  if (err) console.error('[ERROR] SMTP connection failed:', err);
+  else console.log('[DEBUG] SMTP ready to send emails');
+});
+
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
